55732fd5f166f0ecd7c3e0e989143bdffac3712f
peacebonez <kmpariso12@gmail.com>
2020-07-20 22:11:47 -0400

    added gitignore

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const { validationResult } = require("express-validator");
 
 /* GET users listing. */
 router.post("/", [
@@ -11,7 +12,33 @@ router.post("/", [
   ,
   check("password", "Name required").isLength({ min: 6 }),
 
-  async (req, res, next) => {},
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //See if user already exists
+
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+    } catch (err) {}
+  },
 ]);
 
 module.exports = router;
