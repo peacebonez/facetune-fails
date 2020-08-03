492a0850bafeef829af16b4e291fb6b62f74d788
peacebonez <kmpariso12@gmail.com>
2020-08-03 09:33:51 -0400

    still working on buggy pagination

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,36 +1,35 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { getPosts, getMorePosts } from "../actions/post-action";
 import Post from "./Post";
 import Loading from "./Loading";
 
 const Home = ({ post: { posts, loading }, getPosts, getMorePosts }) => {
-  let [page, setPage] = useState(0);
-
+  let [page, setPage] = useState(1);
   let { pageNum } = useParams();
 
   useEffect(() => {
     getPosts();
-  }, [getPosts]);
+    console.log("PAGE NUM USEEFFECT:", pageNum);
+    getMorePosts(pageNum);
+  }, [getPosts, getMorePosts, pageNum]);
 
   const pageUp = () => {
+    // setPage((page) => page + 1);
     setPage(page + 1);
-    if (!pageNum) pageNum = 1;
-    else pageNum++;
     console.log("PAGENUM:", pageNum);
-    console.log(page);
-
-    getMorePosts(pageNum);
+    console.log("CURRENT PAGE:", page);
+    // getMorePosts(pageNum);
   };
   const pageDown = () => {
+    // setPage(() => page - 1);
     setPage(page - 1);
-    pageNum--;
     console.log("PAGENUM:", pageNum);
-    console.log(page);
+    console.log("CURRENT PAGE:", page);
 
-    getMorePosts(pageNum);
+    // getMorePosts(pageNum);
   };
 
   return loading ? (
@@ -43,17 +42,25 @@ const Home = ({ post: { posts, loading }, getPosts, getMorePosts }) => {
       <div
         className="pagination-container"
         style={{
-          justifyContent: `${page === 0 ? "flex-end" : "space-between"}`,
+          justifyContent: `${page === 1 ? "flex-end" : "space-between"}`,
         }}
       >
-        {page > 0 && (
-          <button id="previous-btn" className="btn page-btn" onClick={pageDown}>
-            Previous Page
-          </button>
+        {page > 1 && (
+          <Link to={page > 1 ? `/page-${page}` : ""}>
+            <button
+              id="previous-btn"
+              className="btn page-btn"
+              onClick={pageDown}
+            >
+              Previous Page
+            </button>
+          </Link>
         )}
-        <button id="next-btn" className="btn page-btn" onClick={pageUp}>
-          Next Page
-        </button>
+        <Link to={`/page-${page}`}>
+          <button id="next-btn" className="btn page-btn" onClick={pageUp}>
+            Next Page
+          </button>
+        </Link>
       </div>
     </section>
   );
