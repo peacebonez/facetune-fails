6d6b868653d390b5b2b98e8e86f738c1cb6574fa
peacebonez <kmpariso12@gmail.com>
2020-08-19 17:45:01 -0400

    adjusted get / route

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 const path = require("path");
 
 /* GET home page. */
-router.get("/", (req, res) => {
-  try {
-    res.sendFile(path.join(__dirname + "/client/public/index.html"));
-  } catch (err) {
-    return console.error(err);
-  }
-});
+// router.get("/", (req, res) => {
+//   try {
+//     res.sendFile(path.join(__dirname + "/client/public/index.html"));
+//   } catch (err) {
+//     return console.error(err);
+//   }
+// });
 
 module.exports = router;
