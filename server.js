2088b0e623f0d166a445950901d4bf04bf525a11
peacebonez <kmpariso12@gmail.com>
2020-08-19 17:32:59 -0400

    added direct URI

--- server.js
+++ server.js
@@ -11,12 +11,21 @@ var app = express();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    await mongoose.connect(
+      "mongodb+srv://admin:admin@facetune-fail.lagx6.mongodb.net/facetune-fail?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      }
+    );
+    // await mongoose.connect(process.env.DB_URI, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    // });
     console.log("MongoDB Connected!");
   } catch (err) {
     console.error(err.message);
