dbe0513037d4aa7e8a3af98b0f35e8462aabd854
peacebonez <kmpariso12@gmail.com>
2020-08-19 16:30:34 -0400

    prepare for heroku

--- app.js
+++ app.js
@@ -41,6 +41,16 @@ app.use("/posts", require("./routes/posts"));
 app.use("/users", require("./routes/users"));
 app.use("/auth", require("./routes/auth"));
 
+//Serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
