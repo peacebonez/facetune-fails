78b203f4ffc42bee6b79cbb5c860eb49935663ba
peacebonez <kmpariso12@gmail.com>
2020-08-21 09:43:56 -0400

    added production.json

--- server.js
+++ server.js
@@ -6,22 +6,18 @@ var logger = require("morgan");
 const config = require("config");
 
 const mongoose = require("mongoose");
-require("dotenv").config();
+// require("dotenv").config();
 
 var app = express();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      // "mongodb+srv://admin:admin@facetune-fail.lagx6.mongodb.net/facetune-fail?retryWrites=true&w=majority",
-      config.get("DB_URI"),
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      }
-    );
+    await mongoose.connect(config.get("DB_URI"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
     console.log("Kevin's MongoDB Connected!");
   } catch (err) {
     console.error(err.message);
